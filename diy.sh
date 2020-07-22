bf7182161a54bcc058e16372e7c78cf5d1715a32
qxzg <out@qxzg.xyz>
2020-07-22 15:37:24 +0800

    push

--- diy.sh
+++ diy.sh
@@ -4,7 +4,7 @@ sed -i 's/COMPOSE_PROJECT_NAME=lesspass/COMPOSE_PROJECT_NAME=lesspass-qxzg/g'  c
 sed -i 's/quay.io/'${CONTAINER_DOMAIN}'/g' containers/docker-compose.yml
 sed -i 's/\/lesspass\//'${CONTAINER_PATH}'/g' containers/docker-compose.yml
 echo "">> packages/lesspass-site/Dockerfile
-echo "RUN sed -i 's/https:\/\/lesspass.com/https:\/\/pass.qxzg.xyz/g' /usr/share/nginx/html/lesspass.min.js" >> packages/lesspass-site/Dockerfile
+echo "RUN sed -i 's/https:\/\/lesspass.com/https:\/\/qxzg.xyz/g' /usr/share/nginx/html/lesspass.min.js" >> packages/lesspass-site/Dockerfile
 sed -i 's/EXPOSE 80/EXPOSE 45629/g' packages/lesspass-site/Dockerfile
 sed -i 's/listen       80;/listen       45629;/g' packages/lesspass-site/nginx.conf
 
