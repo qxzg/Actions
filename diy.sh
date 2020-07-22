e96eb2fd485c687b2bea3f7e75877e5ac5af6f20
qxzg <out@qxzg.xyz>
2020-07-22 17:46:09 +0800

    优化dit.sh

--- diy.sh
+++ diy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-sed -i '/^EXPOSE 8000/'d containers/backend/Dockerfile
+#sed -i '/^EXPOSE 8000/'d containers/backend/Dockerfile
 sed -i 's/COMPOSE_PROJECT_NAME=lesspass/COMPOSE_PROJECT_NAME=lesspass-qxzg/g'  containers/.env
 sed -i 's/quay.io/'${CONTAINER_DOMAIN}'/g' containers/docker-compose.yml
 sed -i 's/\/lesspass\//'${CONTAINER_PATH}'/g' containers/docker-compose.yml
-echo "">> packages/lesspass-site/Dockerfile
-echo "RUN sed -i 's/https:\/\/lesspass.com/https:\/\/qxzg.xyz/g' /usr/share/nginx/html/lesspass.min.js" >> packages/lesspass-site/Dockerfile
+echo "">> packages/lesspass-site/Dockerfile;echo "RUN sed -i 's/https:\/\/lesspass.com/https:\/\/qxzg.xyz/g' /usr/share/nginx/html/lesspass.min.js" >> packages/lesspass-site/Dockerfile
+sed -i 's/EXPOSE 8000/EXPOSE 45628/g' containers/backend/Dockerfile
 sed -i 's/EXPOSE 80/EXPOSE 45629/g' packages/lesspass-site/Dockerfile
+sed -i 's/\"0.0.0.0:8000\"/\"0.0.0.0:45628\"/g' containers/backend/Dockerfile
 sed -i 's/listen       80;/listen       45629;/g' packages/lesspass-site/nginx.conf
 
 sed -i 's/\"backend\", \".lesspass.com\", //g' containers/backend/lesspass/settings.py
