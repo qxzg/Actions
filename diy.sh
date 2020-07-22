65c84d7fa4f89970b713593bbcfbee04067deef9
qxzg <out@qxzg.xyz>
2020-07-22 10:40:17 +0800

    test

--- diy.sh
+++ diy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 sed -i '/^EXPOSE 8000/'d containers/backend/Dockerfile
 sed -i 's/COMPOSE_PROJECT_NAME=lesspass/COMPOSE_PROJECT_NAME=lesspass-qxzg/g'  containers/.env
+sed -i 's/quay.io/${CONTAINER_DOMAIN}/g' containers/docker-compose.yml
+sed -i 's/\/lesspass\/${CONTAINER_PATH}/g' containers/docker-compose.yml
 
 sed -i 's/EXPOSE 80/EXPOSE 45629/g' packages/lesspass-site/Dockerfile
 sed -i 's/listen       80;/listen       45629;/g' packages/lesspass-site/nginx.conf 
+
+
+
