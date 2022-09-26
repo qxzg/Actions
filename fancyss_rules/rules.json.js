a5070936897ed802a115997298e0168f41737ac6
qxzg <30212669+qxzg@users.noreply.github.com>
2022-09-26 16:17:16 +0800

    Update rules.json.js

---
+++ fancyss_rules/rules.json.js
@@ -0,0 +1,59 @@
+{
+  "gfwlist": {
+    "name": "gfwlist.conf",
+    "date": "2022-07-16 23:02",
+    "md5": "5f798c92b910647f18a706d3eb738242",
+    "count": "5598"
+  },
+  "chnroute": {
+    "name": "chnroute.txt",
+    "date": "2022-07-16 23:02",
+    "md5": "005e9b0d43299ec972f4a1ffb8d3abf7",
+    "count": "3441",
+    "count_ip": "298474270",
+    "source": "misakaio",
+    "url": "https://github.com/misakaio/chnroutes2/blob/master/chnroutes.txt"
+  },
+  "chnroute2": {
+    "name": "chnroute2.txt",
+    "date": "2022-07-16 23:02",
+    "md5": "f12a5a7fb6c9c37d52dbbea0666812ba",
+    "count": "6182",
+    "count_ip": "13240665434",
+    "source": "ipip",
+    "url": "https://github.com/firehol/blocklist-ipsets/blob/master/ipip_country/ipip_country_cn.netset"
+  },
+  "chnroute3": {
+    "name": "chnroute3.txt",
+    "date": "2022-07-16 23:02",
+    "md5": "b3d88fa1fcc1e9ea1c1b4c9b2df3cc18",
+    "count": "8637",
+    "count_ip": "343307398",
+    "source": "apnic",
+    "url": "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+  },
+  "cdn_china": {
+    "name": "cdn.txt",
+    "date": "2022-07-16 23:02",
+    "md5": "12ca527bc203b953c9a0e71ebdc5de58",
+    "count": "66083"
+  },
+  "apple_china": {
+    "name": "apple_china.txt",
+    "date": "2022-07-16 23:02",
+    "md5": "f9de2c1c862bc12d5d96e350c4fc8991",
+    "count": "124"
+  },
+  "google_china": {
+    "name": "google_china.txt",
+    "date": "2022-07-16 23:02",
+    "md5": "24986fc4b1d9216b5e24b7e54100df3d",
+    "count": "71"
+  },
+  "cdn_test": {
+    "name": "cdn_test.txt",
+    "date": "2022-07-16 23:02",
+    "md5": "6238d54f2bb2537088ac8e0f645a4386",
+    "count": "70"
+  }
+}
