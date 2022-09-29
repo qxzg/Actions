1122b0ee43ea42e9dc5431003baf5ed9d3e6b3a6
qxzg <30212669+qxzg@users.noreply.github.com>
2022-09-29 04:36:08 +0000

    update md5 and date in rules.json.js

--- fancyss_rules/rules.json.js
+++ fancyss_rules/rules.json.js
@@ -1,13 +1,13 @@
 {
   "gfwlist": {
     "name": "gfwlist.conf",
-    "date": "2022-07-16 23:02",
-    "md5": "5f798c92b910647f18a706d3eb738242",
+    "date": "2022-09-29 12:32",
+    "md5": "3e60b057846be5da57d597128a769b43",
     "count": "5598"
   },
   "chnroute": {
     "name": "chnroute.txt",
-    "date": "2022-09-29 03:50",
+    "date": "2022-09-29 12:32",
     "md5": "fc16f9a9ea0fc3c349a53c005b3ed0b2",
     "count": "4239",
     "count_ip": "265620706",
@@ -16,7 +16,7 @@
   },
   "chnroute2": {
     "name": "chnroute2.txt",
-    "date": "2022-07-16 23:02",
+    "date": "2022-09-29 12:32",
     "md5": "f12a5a7fb6c9c37d52dbbea0666812ba",
     "count": "6182",
     "count_ip": "13240665434",
@@ -25,7 +25,7 @@
   },
   "chnroute3": {
     "name": "chnroute3.txt",
-    "date": "2022-09-27 03:48",
+    "date": "2022-09-29 12:32",
     "md5": "06cda8d6a7ebffb6da9c5eb946f3920a",
     "count": "8627",
     "count_ip": "343232410",
@@ -34,26 +34,26 @@
   },
   "cdn_china": {
     "name": "cdn.txt",
-    "date": "2022-09-29 03:50",
-    "md5": "dcd4b44ce224257b8293b2acd643f0d4",
+    "date": "2022-09-29 12:32",
+    "md5": "4e594098a1e9385dc110cd5720733f8e",
     "count": "65392"
   },
   "apple_china": {
     "name": "apple_china.txt",
-    "date": "2022-07-16 23:02",
-    "md5": "f9de2c1c862bc12d5d96e350c4fc8991",
-    "count": "124"
+    "date": "2022-09-29 12:32",
+    "md5": "2a5f92d2a47cc281190d11db4a28427c",
+    "count": "135"
   },
   "google_china": {
     "name": "google_china.txt",
-    "date": "2022-07-16 23:02",
+    "date": "2022-09-29 12:32",
     "md5": "24986fc4b1d9216b5e24b7e54100df3d",
     "count": "71"
   },
   "cdn_test": {
     "name": "cdn_test.txt",
-    "date": "2022-07-16 23:02",
-    "md5": "6238d54f2bb2537088ac8e0f645a4386",
-    "count": "70"
+    "date": "2022-09-29 12:32",
+    "md5": "0a62118fad3d440a031f318c4cf45953",
+    "count": "73"
   }
 }
